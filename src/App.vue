c02febae629c1ef2af85de9ad97730f9cd92212c
alaanegm <43115408+alaanegm@users.noreply.github.com>
2022-03-31 22:03:06 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <!-- <router-link to="/">Home</router-link>  -->
+  </nav>
+  
+  <router-view/>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+body {
+ 
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+  background:#f8f8f8;
+}
+
+#app {
+  font-family: Montserrat,Helvetica,Arial,serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+// nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+</style>
